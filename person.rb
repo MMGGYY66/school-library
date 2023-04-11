a8131e4df78795049a1cafb089ec1e9d92406d66
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-11 18:03:41 -0500

    Update Person class to inherit from Nameable and create a method correct_name implemented to simply return the name attribute.

--- person.rb
+++ person.rb
@@ -1,12 +1,20 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()  
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  # a method correct_name implemented to simply return the name attribute.
+  def correct_name
+    @name
   end
 
   def can_use_services?
