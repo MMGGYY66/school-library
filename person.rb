7a1921dd826bbd4a9673abacb4ff6352cbfb1412
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-11 19:29:05 -0500

    Run rubocop and fix linters issues

--- person.rb
+++ person.rb
@@ -3,8 +3,9 @@ require_relative './nameable'
 class Person < Nameable
   attr_accessor :name, :age, :id
   attr_reader :parent_permission
-  
+
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
