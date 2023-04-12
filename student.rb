d17d20aa718df3f86259b737efb9b8eaf488dfaf
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-04-12 15:23:12 -0500

    Update student.rb to Create the has-many/belongs-to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -2,13 +2,20 @@ require_relative './person'
 
 class Student < Person
   attr_accessor :classroom
+  attr_reader :json_class
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true, json_class: 'Student')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @json_class = json_class
+  end
+
+  def classrooms=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    ' ¯\\(ツ)/¯ '
   end
 end
