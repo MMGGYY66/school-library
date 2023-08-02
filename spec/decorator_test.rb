9a3249ebe40657460cb8eff91e7cf5459f877d08
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-02 18:24:09 +0300

    Create tests 4

--- spec/decorator_test.rb
+++ spec/decorator_test.rb
@@ -6,7 +6,7 @@ require_relative '../person'
 describe Decorator do
   person = Person.new(22, 'nacho_mohamed')
   it 'teats base decorator' do
-    expect(person.correct_name).to eql('maximilianus')
+    expect(person.correct_name).to eql('nacho_mohamed')
   end
   it 'tests the capitalization decorator' do
     capitalized_person = CapitalizeDecorator.new(person)
